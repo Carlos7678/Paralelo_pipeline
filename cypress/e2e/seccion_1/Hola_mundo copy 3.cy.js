0bd8574a471754eea0134563a5c1be3b9bd9bf0f
Carlos <carloscontacto4d@gmail.com>
2024-04-03 16:57:43 +0200

    otra vez

--- cypress/e2e/seccion_1/Hola_mundo copy 3.cy.js
+++ cypress/e2e/seccion_1/Hola_mundo copy 3.cy.js
@@ -5,13 +5,13 @@ describe("Bienvenidos al curso de Cypress sección 1", () => {
 
   it("Mi primer test-> Hola mundo", () => {
     cy.log("Hola mundo");
-    cy.wait(1500);
+    
   });
 
   it("Segundo test -> campo name", () => {
     cy.visit("https://demoqa.com/text-box");
     cy.get("#userName").type("Carlos");
-    cy.wait(1500);
+    
   });
   
 });
